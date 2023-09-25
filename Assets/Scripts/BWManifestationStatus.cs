b3d52c9b37fae73cd22dd07dab718a95d7446f86
Evelyn Ramos <eveh.rms7@gmail.com>
2023-09-25 17:15:11 -0300

    add-mechanic1-v01

--- Assets/Scripts/BWManifestationStatus.cs
+++ Assets/Scripts/BWManifestationStatus.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class BWManifestationStatus : State
 {
-    // Start is called before the first frame update
+    public GameObject BWPrefab;
+
+    public float manifestationDuration = 2f; //Duracion de la manifestacion
+    private float manifestationTimer = 0f; //Temporizador para rastrear el tiempo de la manifestacion
+    private bool isManifested = false; //Es6ta siendo manifestada en falso
+
+    //public List<Transform> spawnPoints; //Lista de puntos de aparicion
+    /* [SerializeField]
+    private Transform[] WayPoints; */
+    public Transform[] WayPoints;//Este array contiene los waiPoints
+    private GameObject currentBWInstance; // Variable para mantener una referencia a la instancia actual
+    
     protected override void Start()
     {
         // Logica de inicializacion especifica de BWManifestationStatus
@@ -12,16 +23,59 @@ public class BWManifestationStatus : State
     }
     public override void EnterState()
     {
-        // Logica de entrada al estado BWManifestationStatus
+        manifestationTimer = 0f;
+        isManifested = false;
+
+        //Metodo para manifestar viuda
+        ManifestBlackWidow();
     }
 
     public override void UpdateState()
     {
-        // Logica de actualizacion del estado BWManifestationStatus
+        if (isManifested)
+        {
+           manifestationTimer += Time.deltaTime;
+           if(manifestationTimer >= manifestationDuration)
+           {
+            DisappearBlackWidow();
+            manifestationTimer = 0f; // Reiniciar el temporizador
+            ManifestBlackWidow();
+           }
+            
+        }
+        
     }
 
     public override void ExitState()
     {
-        // Logica de salida del estado BWManifestationStatus
+        
+    }
+    private void ManifestBlackWidow()
+    {
+        if (WayPoints.Length > 0)
+        {
+            int ramdomSpawnIndex = Random.Range(0, WayPoints.Length);
+            Transform WayPoint = WayPoints[ramdomSpawnIndex];
+            
+            // Destruir la instancia actual si existe
+            if (currentBWInstance != null)
+            {
+                Destroy(currentBWInstance);
+            }
+
+            currentBWInstance = Instantiate(BWPrefab, WayPoint.position, WayPoint.rotation);
+
+            isManifested = true;
+        }
+    }
+    private void DisappearBlackWidow()
+    {
+        // Destruir la instancia actual si existe
+        if (currentBWInstance != null)
+        {
+            Destroy(currentBWInstance);
+        }
+        
+        isManifested = false; // Cambiar a falso para que pueda manifestarse de nuevo en el siguiente ciclo
     }
 }
